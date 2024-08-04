ed15e3cacf86bd476b92e8eda7404e38e852caa5
___________ <86606979+Very-Blank@users.noreply.github.com>
2024-08-04 12:21:10 +0300

    Create ITR2.lua

---
+++ ITR2.lua
@@ -0,0 +1,29 @@
+--#region Levels
+    --#region Forest
+
+    --#endregion
+    --#region Peninsula
+
+    --#endregion
+    --#region Hub
+
+    --#endregion
+
+    --#region Common
+        --#region Functions
+        RegisterHook("/Script/IntoTheRadius2.RadiusGameInstance:OnLevelLoaded", function()
+        end, function ()
+        end)
+        RegisterHook("/Script/IntoTheRadius2.RadiusGameInstance:OnGameStart", function()
+        end, function ()
+        end)
+        RegisterHook("/Script/IntoTheRadius2.RadiusGameInstance:ExitGame", function()
+        end, function ()
+        end)
+        --#endregion
+    --#endregion
+
+--#endregion
+
+--#region Items
+--#endregion
