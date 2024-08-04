1ed184e315fa338f4f5c5fe6b4f02089562d2c7c
___________ <86606979+Very-Blank@users.noreply.github.com>
2024-08-04 20:57:22 +0300

    Update ITR2.lua
    Added more functions

--- ITR2.lua
+++ ITR2.lua
@@ -11,9 +11,20 @@
 
     --#region Common
         --#region Functions
+
+        ---@param LevelTag FGameplayTag
+        RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:OnLoadLevel", function(LevelTag)
+        end, function()
+        end)
+
         RegisterHook("/Script/IntoTheRadius2.RadiusGameInstance:OnLevelLoaded", function()
         end, function ()
         end)
+
+        RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:OnLevelLoaded", function()
+        end, function()
+        end)
+        
         RegisterHook("/Script/IntoTheRadius2.RadiusGameInstance:OnGameStart", function()
         end, function ()
         end)
@@ -26,4 +37,14 @@
 --#endregion
 
 --#region Items
+    --Notes:
+    --When modifying Items you should use NotifyOnNewObject after level has been loaded
+--#endregion
+
+--#region Player
+    --#region Functions
+    ---@param Controller AController
+    RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:Server_RespawnPlayer", function(Controller)
+    end)
+    --#endregion
 --#endregion
