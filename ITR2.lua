957ef594c7e49974e72efd215aba457b8cd2572a
Merith <merith.tk@gmail.com>
2024-08-04 20:05:46 +0000

    redo a large part of the structure

--- ITR2.lua
+++ ITR2.lua
@@ -1,76 +1,66 @@
-local ITR2 = {}
-ITR2.version = "0.10.2" -- version this was last updated for
-ITR2.Hooks = {}
+local ITR2 = {
+    version = "0.10.2", -- version this was last updated for
 
---#region Levels
-    --#region Forest
+    GameInstance = {
+        ---@param func function
+        OnLevelLoaded = function(func)
+            RegisterHook("/Script/IntoTheRadius2.RadiusGameInstance:OnLevelLoaded", func, function() end)
+        end,
 
-    --#endregion
-    --#region Peninsula
+        ---@param func function
+        OnGameStart = function(func)
+            RegisterHook("/Script/IntoTheRadius2.RadiusGameInstance:OnGameStart", func, function() end)
+        end,
 
-    --#endregion
-    --#region Hub
+        ---@param func function
+        ExitGame = function(func)
+            RegisterHook("/Script/IntoTheRadius2.RadiusGameInstance:ExitGame", func, function() end)
+        end
+    },
 
-    --#endregion
-
-    --#region Common
-        --#region Functions
-
+    GameMode = {
         ---@param LevelTag FGameplayTag
         ---@param func function
-        function ITR2.Hooks.OnLoadLevel(LevelTag, func)
+        OnLoadLevel = function(LevelTag, func)
             RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:OnLoadLevel", function(LevelTag)
                 func()
-            end, function()
-            end)
-        end
+            end, function() end)
+        end,
 
         ---@param func function
-        function ITR2.Hooks.OnLevelLoaded(func)
-            RegisterHook("/Script/IntoTheRadius2.RadiusGameInstance:OnLevelLoaded", func, function()
-            end)
-            
-            RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:OnLevelLoaded", func, function()
-            end)
+        OnLevelLoaded = function(func)
+            RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:OnLevelLoaded", func, function() end)
         end
+    },
 
+    Levels = {
+        Forest = {},
+
+        Peninsula = {},
+
+        Hub = {}
+    },
+
+    Items = {
+        Backpacks = {
+            -- Medium Firepower
+            -- /Game/ITR2/BPs/Items/Equipment/Backpacks/BP_BackPack_Medium_2.BP_BackPack_Medium_2_C
+            -- Medium Pouch
+            -- /Game/ITR2/BPs/Items/Equipment/Backpacks/BP_BackPack_Medium_1.BP_BackPack_Medium_1_C
+        }
+        -- Notes:
+        -- When modifying Items you should use NotifyOnNewObject after level has been loaded
+    },
+
+    Player = {
+        ---@param Controller AController
         ---@param func function
-        function ITR2.Hooks.OnGameStart(func)
-            RegisterHook("/Script/IntoTheRadius2.RadiusGameInstance:OnGameStart", func, function()
+        RespawnPlayer = function(Controller, func)
+            RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:Server_RespawnPlayer", function(Controller)
+                func()
             end)
         end
-
-        ---@param func function
-        function ITR2.Hooks.ExitGame(func)
-            RegisterHook("/Script/IntoTheRadius2.RadiusGameInstance:ExitGame", func, function()
-            end)
-        end
-        --#endregion
-    --#endregion
-
---#endregion
-
---#region Items
-    --#region Backpacks
-        -- Medium Firepower
-        -- /Game/ITR2/BPs/Items/Equipment/Backpacks/BP_BackPack_Medium_2.BP_BackPack_Medium_2_C
-        -- Medium Pouch
-        -- /Game/ITR2/BPs/Items/Equipment/Backpacks/BP_BackPack_Medium_1.BP_BackPack_Medium_1_C
-    --#endregion
-    --Notes:
-    --When modifying Items you should use NotifyOnNewObject after level has been loaded
---#endregion
-
---#region Player
-    --#region Functions
-    ---@param Controller AController
-    ---@param func function
-    function ITR2.Hooks.RespawnPlayer(Controller, func)
-        RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:Server_RespawnPlayer", function(Controller)
-            func()
-        end)
-    end
-    --#endregion
---#endregion
+    }
+}
 
 return ITR2
