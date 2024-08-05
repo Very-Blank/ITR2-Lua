cd73c47334b4db42bea603956bfe4d3c1840ff2d
aapeli <aapeli.saarelainen.76@gmail.com>
2024-08-05 09:41:15 +0300

    Fixed few issues and added items

--- ITR2.lua
+++ ITR2.lua
@@ -19,18 +19,21 @@ local ITR2 = {
     },
 
     GameMode = {
-        ---@param LevelTag FGameplayTag
+        --Register hook calls this with LevelTag
         ---@param func function
-        OnLoadLevel = function(LevelTag, func)
-            RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:OnLoadLevel", function(LevelTag)
-                func()
-            end, function() end)
+        OnLoadLevel = function(func)
+            ---@param LevelTag FGameplayTag
+            RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:OnLoadLevel", func, function() end)
         end,
 
         ---@param func function
         OnLevelLoaded = function(func)
             RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:OnLevelLoaded", func, function() end)
         end
+    },
+
+    EventSystem = {
+
     },
 
     Levels = {
@@ -42,23 +45,29 @@ local ITR2 = {
     },
 
     Items = {
-        Backpacks = {
-            -- Medium Firepower
-            -- /Game/ITR2/BPs/Items/Equipment/Backpacks/BP_BackPack_Medium_2.BP_BackPack_Medium_2_C
-            -- Medium Pouch
-            -- /Game/ITR2/BPs/Items/Equipment/Backpacks/BP_BackPack_Medium_1.BP_BackPack_Medium_1_C
-        }
         -- Notes:
-        -- When modifying Items you should use NotifyOnNewObject after level has been loaded
+        -- When modifying Items you should use NotifyOnNewObject after the level has been loaded
+        Backpacks = {
+            Medium2 = "/Game/ITR2/BPs/Items/Equipment/Backpacks/BP_BackPack_Medium_2.BP_BackPack_Medium_2_C",
+            Medium1 = "/Game/ITR2/BPs/Items/Equipment/Backpacks/BP_BackPack_Medium_1.BP_BackPack_Medium_1_C",
+        },
+
+        Guns = {
+            Saiga = "/Game/ITR2/BPs/Items/Firearms/Primary/Saiga/BP_Saiga.BP_Saiga_C",
+        },
+
+        Magazines = {
+            SaigaShort = "/Game/ITR2/BPs/Items/Firearms/Primary/Saiga/BP_Saiga_Mag_Short2.BP_Saiga_Mag_Short2_C",
+            SaigaLong = "/Game/ITR2/BPs/Items/Firearms/Primary/Saiga/BP_Saiga_Mag_Long.BP_Saiga_Mag_Long_C",
+        },
     },
 
     Player = {
-        ---@param Controller AController
+        --Register hook calls this with Controller
         ---@param func function
-        RespawnPlayer = function(Controller, func)
-            RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:Server_RespawnPlayer", function(Controller)
-                func()
-            end)
+        RespawnPlayer = function(func)
+            ---@param Controller AController
+            RegisterHook("/Script/IntoTheRadius2.RadiusGameMode:Server_RespawnPlayer", func)
         end
     }
 }
