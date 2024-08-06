fd4f36625c774ae2803c5ea6e663f05918e69789
aapeli <aapeli.saarelainen.76@gmail.com>
2024-08-06 13:49:36 +0300

    BUG with path name missing /

--- ITR2.lua
+++ ITR2.lua
@@ -2,7 +2,7 @@
 -- define shorthand paths for common paths
 local paths = {
     Item = "/Game/ITR2/BPs/Items/",
-    Firearms = "/Game/ITR2/BPs/Items/Firearms",
+    Firearms = "/Game/ITR2/BPs/Items/Firearms/",
     Script = "/Script/IntoTheRadius2."
 }
 local ITR2 = {
